17dce00eb50c72dfbe3128cb00e413490501d7c3
kenjinp <kenjin.p@gmail.com>
2014-12-06 18:07:06 -0600

    finished data input to hash, made code betterer

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-THIS IS A POPULATOR, I dont know how it works yet.
+THIS IS A POPULATOR!
+it takes data from your gmail, then inputs that data into a database
 
-The config file is in this format:
+You can choose to only search labeled emails, or all of them.
+
+The data parsed is taken care of in the parse method.
+
+
+
+The config file uses YAML (named 'config.yaml') and is in this format:
 config:
   username: 'yourusername'
   password: 'yourpassword'
   label: 'label of emails to look up' (if 'none', then search entire mailbox)
+  peek: 'true'/other (if true, emails parsed won't be labeled as read)
--- inboxer.rb
+++ inboxer.rb
@@ -1,66 +1,92 @@
 require 'gmail'
 require 'yaml'
 require 'nokogiri'
-require 'open-uri'
 
-class Order
-  attr_accessor :name,
-                :title,
-                :cell_number,
-                :ambit_handle,
-                :image,
-                :coupon_code
-
-  def initialize(name, title, cell_number, ambit_handle, image, coupon_code)
-    @name = name
-    @title = title
-    @cell_number = cell_number
-    @ambit_handle = ambit_handle
-    @image = image
-    @coupon_code = coupon_code
-  end
-
-end
-
-def read_config
+def readConfig
   config = YAML.load_file("config.yaml")
   @username = config["config"]["username"]
   @password = config["config"]["password"]
   @label = config["config"]["label"]
+  @peek = config["config"]["peek"]
 end
 
-read_config
+def parser(email)
+  #create the parsable doc
+  doc = Nokogiri::HTML(email.body.to_s)
+  table = doc.at('table')
 
-begin
-  puts 'attempting to connect to ' + @username + '@gmail.com'
-  Gmail.new(@username, @password) do |gmail|
-      puts 'connection established'
-      #use peek to make emails not be automatically marked as read
-      gmail.peek = true
-      if @label == "none"
-        inbox = gmail.inbox
-        boxname = 'inbox'
-      else
-        inbox = gmail.mailbox(@label)
-        boxname = @label + ' box'
-      end
-      number_unread = inbox.count(:unread)
-      if number_unread < 1
-        abort("There are no new messages in the " + boxname)
-      end
-      puts "there are " + number_unread.to_s + " unread emails in the " + boxname
-      inbox.emails(:unread).each do |email|
-        puts '///' * 30
-        #puts email.body
-        doc = Nokogiri::HTML(email.body.to_s)
-        table = doc.at('table')
-        table.search('b').each do |t|
-          puts t.text
-          puts t.next.next.text
-        end
-      end
+  #read the text associated with the bold text in an email
+  def textAtNode(node, number)
+    #text may not be incoded correctly, may try to .force_encoding in the future
+    return node.css('b')[number].next.next.text.to_s.gsub("=\n", '').gsub("=0A", '')
   end
 
-rescue Net::IMAP::NoResponseError
-  puts 'connection failed, check your credentials and connectivity'
+  #this is the only exceptional peice of data that must be treated with special cases
+  if textAtNode(table, 5).include? 'Use the file that I upload (below) OR please use:'
+    # if they chose to upload their own file, use the link
+    image = textAtNode(table, 6)
+    # if they didn't upload an image so there is no link, choose a default for them
+    image = (image === '' ? 'Free Energy' : image)
+  else
+    #otherwise they must have chosen a default image, so just use that text
+    image = textAtNode(table, 5)
+  end
+
+  email_data = {
+    'name' => textAtNode(table, 0),
+    'title' => textAtNode(table, 1),
+    'cell' => textAtNode(table, 2),
+    'customer_email' => textAtNode(table, 3),
+    'ambit_handle' => textAtNode(table, 4),
+    'image' => image,
+    'alternate_video' => textAtNode(table, 7),
+    'alternate_keyword' => textAtNode(table, 8),
+    'alternate_web' => textAtNode(table, 9),
+    'coupon_code' => textAtNode(table, 10),
+    'referral' => textAtNode(table, 11)
+  }
+
+  email_data.each do |item, data|
+    #convert empty strings to nil
+    data = (data.empty? ? nil : data)
+  end
+
+  return email_data
+
+end #end parser
+
+#set up gmail connection, preforms something on each unread email,
+#set peek to true if you don't want to automatically mark it as read
+def getEmails
+  begin
+    puts 'attempting to connect to ' + @username + '@gmail.com'
+    Gmail.new(@username, @password) do |gmail|
+        puts 'connection established'
+        #use peek to make emails not be automatically marked as read
+        gmail.peek = (@peek === 'true' ? true : false)
+
+        if @label == "none"
+          inbox = gmail.inbox
+          boxname = 'inbox'
+        else
+          inbox = gmail.mailbox(@label)
+          boxname = @label + ' box'
+        end
+        number_unread = inbox.count(:unread)
+        if number_unread < 1
+          abort("There are no new messages in the " + boxname)
+        end
+        p "there are " + number_unread.to_s + " unread emails in the " + boxname
+        inbox.emails(:unread).each do |email|
+          puts parser(email)
+        end
+    end
+
+  rescue Net::IMAP::NoResponseError
+    puts 'connection failed, check your config file for incorrect credentials'
+    puts 'perhaps you opted to use a label that doesn\'t exist, try "none" instead'
+  end
 end
+
+readConfig
+getEmails
